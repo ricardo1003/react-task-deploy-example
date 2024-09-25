af25a41ddcd04775b72f5277673e846695f3f4e2
unknown <ricardodonado1105@gmail.com>
2024-09-24 22:21:12 -0500

    Mostrar lista de tareas

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks =[
+    {
+        id:0,
+        title: "mi primer tarea",
+        description: "en efecto, es mi primer tarea"
+    },
+    {
+        id:1,
+        title: "mi segunda tarea",
+        description: "en efecto, es mi primer tarea"
+    },
+    {
+        id:2,
+        title: "mi tercera tarea",
+        description: "en efecto, es mi primer tarea"
+    },
+]
